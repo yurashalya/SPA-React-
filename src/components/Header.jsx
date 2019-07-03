a7e7b3134723ae2abdb1eaf72bf899068aab9559
Yura Shalya <shalyayura@gmail.com>
2019-07-03 22:59:34 +0300

    Create project / First commit / add structure and components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png" />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+          <div>
+            <img src="https://www.ateamindia.com/wp-content/uploads/2019/04/web-dev.jpg" />
+          </div>
+          <div>
+            ava + descr
+          </div>
+          <div> 
+              My Post
+              <div>
+                New Post
+              </div>
+              <div>Post 1 </div>
+              <div>Post 2</div>
+          </div>
+        </div>
+    )
+}
+
+export default Profile;
