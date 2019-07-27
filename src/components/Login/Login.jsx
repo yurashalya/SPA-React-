c16e48c207e7abba4c5fb322afcc156c5e17cd84
Shalya Yura <stallker9696@gmail.com>
2019-07-27 08:03:35 +0300

    Connect Redux-form, create login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field type="text" placeholder={'Login'} component={'input'} name={'login'}/>
+            </div>
+            <div>
+                <Field type="password" placeholder={'Password'} component={'input'} name={'password'}/>
+            </div>
+            <div>
+                <Field type="checkbox" component={'input'} name={'rememberMe'}/> Remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+};
+
+const LoginReduxForm = reduxForm({ form: 'login'})(LoginForm);
 
 const Login = (props) => {
-    return <h1>LOGIN</h1>
+
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+
+    return <div>
+        <h1>LOGIN</h1>
+        <LoginReduxForm onSubmit={onSubmit} />
+    </div>
 }
 
 export default Login;
